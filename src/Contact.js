b9d2e38d433d9890dc587f44f8825444d4ccf4a5
Sravanthi <sravanthi.cb@gmail.com>
2018-10-11 11:58:59 -0400

    Initial commit

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+	render() {
+	  return (	
+		<div>
+		   <h2>Contact</h2>
+		   <p>This page is related to contact page</p>
+		</div>
+		);
+	}
+}
+
+export default Contact;
