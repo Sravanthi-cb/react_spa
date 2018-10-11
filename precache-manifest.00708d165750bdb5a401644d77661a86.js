38c75642bdb9e90dada8e6c13773b3c00815c0c6
Sravanthi <sravanthi.cb@gmail.com>
2018-10-11 12:06:48 -0400

    Updates

---
+++ precache-manifest.00708d165750bdb5a401644d77661a86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e69d20ec678e3184859c",
+    "url": "/react_spa/static/js/runtime~main.e69d20ec.js"
+  },
+  {
+    "revision": "abdab0f14fc46f2850e6",
+    "url": "/react_spa/static/js/main.abdab0f1.chunk.js"
+  },
+  {
+    "revision": "00fc6f07ecbcb43128d2",
+    "url": "/react_spa/static/js/1.00fc6f07.chunk.js"
+  },
+  {
+    "revision": "abdab0f14fc46f2850e6",
+    "url": "/react_spa/static/css/main.954353b1.chunk.css"
+  },
+  {
+    "revision": "a066f9f4832689f673d2297e7d352665",
+    "url": "/react_spa/index.html"
+  }
+];
